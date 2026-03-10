74c0f9b64b95879076c2da5c55f1897cc7ea5646
VictorOmboke <victoromboke@gmail.com>
2026-03-09 19:57:16 -0700

    Initial commit.

---
+++ Proj6_ombokev.asm
@@ -0,0 +1,32 @@
+TITLE MASM Temperature Organizer   (Proj6_ombokev.asm)
+
+; Author: 
+; Last Modified:
+; OSU email address: ONID_ID@oregonstate.edu
+; Course number/section:   CS271 Section ???
+; Project Number:                 Due Date:
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+
+.data   ; Data Segment BETA
+
+
+.code
+main PROC
+  
+
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+	
+
+END main
